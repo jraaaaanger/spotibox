2eef1f230b4d029316f449bab95162a761fcd7c1
Jimmy Ranger <jranger@me.com>
2016-08-18 13:16:52 -0400

    adds playlist controller

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,23 @@
 class PlaylistsController < ApplicationController
+  def new
+    @playlist = Playlist.new
+  end
+
+  def create
+    @playlist = Playlist.new(new_playlist_params)
+  end
+
+  def index
+    @playlists = Playlist.all
+  end
+
+  def show
+    @playlist = Playlist.find(param[:playlist_id])
+  end
+
+  private
+
+  def new_playlist_params
+    params.require(:playlist).permit(:name, :theme)
+  end
 end
